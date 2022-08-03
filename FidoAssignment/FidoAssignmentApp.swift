b5a93f2554ba015c8ead09cd47ea8f4f38036408
Aaron Adelman <Aaron.Solomon.Adelman@gmail.com>
2022-08-03 09:40:57 +0300

    Initial Commit

---
+++ FidoAssignment/FidoAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidoAssignmentApp.swift
+//  FidoAssignment
+//
+//  Created by אהרן שלמה אדלמן on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FidoAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
